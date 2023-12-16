693d3ddfc56a42984505729a8d74addcdc83a2b8
bordplate <vetle@bordplate.no>
2023-12-16 21:09:13 +0100

    Change substituted function

--- config_RC3.mk
+++ config_RC3.mk
@@ -6,8 +6,8 @@
 
 TOC									= 0xc0a4e0 # static throughout
 LOADER_INJECT_ADDR 					= 0x9764bc # 0x014D4C # nop in main()
-LOADER_START_ADDR 					= 0x20f560  # 0x20f558 + 4 + align16
-LOADER_END_ADDR 					= 0x20F670 # LOADER_START_ADDR + 0x500
+LOADER_START_ADDR 					= 0xa958c0  # 0x20f558 + 4 + align16
+LOADER_END_ADDR 					= 0xA959c0 # LOADER_START_ADDR + 0x100
 LOADER_SYS_PRX_MODULE_LOAD_ADDR 	= 0xaef624 # cellPrxForUser sys_prx_load_module
 LOADER_SYS_PRX_MODULE_START_ADDR 	= 0xaef784 # cellPrxForUser _sys_process_at_Exitspawn 0x96328741
 
@@ -15,7 +15,7 @@ LOADER_SCE_NP_DRM_IS_AVAILABLE_ADDR	= 0xaeef64 # sceNp sceNpDrmIsAvailable2
 
 # 1414 instructions, around ~375 hooks
 HOOK_SHARED_TEXT_BEGIN_ADDR = $(LOADER_END_ADDR)
-HOOK_SHARED_TEXT_END_ADDR 	= 0x2103c0	 # HOOK_SHARED_TEXT_BEGIN_ADDR + 0x1588
+HOOK_SHARED_TEXT_END_ADDR 	= 0xa96ecc	 # HOOK_SHARED_TEXT_BEGIN_ADDR + 0x1588
 
 # 4 bytes
 HOOK_SHARED_DATA_BEGIN_ADDR = 0xbd1050
